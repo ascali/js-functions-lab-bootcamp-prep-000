ce22db8cb6d093f38cf7d341a3ebf69d9b51405f
Ascaliko <ascaliko7@gmail.com>
2019-10-02 06:34:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(string){
+  var str = "Happy holidays!"
+  return str
+}
+
+function happyHolidaysTo(string, name){
+  var str = `Happy holidays, ${name}!`
+  return str
+}
